2e5262cddd655cee32d88540204846ae25628cf4
Sarah Rainsberger <5098874+sarah11918@users.noreply.github.com>
2021-03-04 12:14:01 -0400

    set up siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `sGatsby`,
+    description: `sarah11918 doin' Gatsby`,
+    author: `@sarah11918`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
