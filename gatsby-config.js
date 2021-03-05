f023e7feb27d299dececf27c892542d988f0629b
Sarah Rainsberger <5098874+sarah11918@users.noreply.github.com>
2021-03-05 16:34:21 -0400

    fixed typo

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
-      },
+
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
