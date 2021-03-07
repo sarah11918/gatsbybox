e18ab95fe6f8e228a4b0874eba58480ddc038f30
Sarah Rainsberger <5098874+sarah11918@users.noreply.github.com>
2021-03-07 11:03:53 -0400

    updated gatsby-node to generate pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,5 +11,35 @@ const { createFilePath } = require(`gatsby-source-filesystem`)
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
   if (node.internal.type === `Mdx`) {
+    const slug = createFilePath({node, getNode,basePath: `pages`})
+    createNodeField({node, name:`slug`,value: slug})
   }
+}
+
+export.createPages = async({graphql,actions}) => {
+  const {createPAge} = actions
+  const result = await graphql(`
+    query {
+      allMdx{
+        edges{
+          node{
+            fields{
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+    result.data.allMdx.edges.forEach(({node}) => {
+        createPage({
+          path:node.fields.slug,
+          component:path.resolve(`./src/templates/blog-post.js`),
+          context: {
+            slug:node.fields.slug,
+          }
+        })
+    })
+      
 }
